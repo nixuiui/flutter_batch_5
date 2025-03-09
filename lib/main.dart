de8f2443ff1766d5b8de7557cbc897ff12b4da51
Niki Rahmadi Wiharto <nikirahmadiwiharto@gmail.com>
2025-03-09 16:48:02 +0700

    feat: column & row

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_batch_5/pages/home_page.dart';
+import 'package:flutter_batch_5/pages/layout_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: LayoutPage(),
     );
   }
 }
